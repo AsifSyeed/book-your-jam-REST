4a61073eb6a0994930a390d92737e9a46f45fcde
Asif Syeed <asifsyeed666@gmail.com>
2023-06-03 23:57:40 +0600

    Initial Commit

---
+++ src/main/java/com/example/BookYourJam/BookYourJamApplication.java
@@ -0,0 +1,13 @@
+package com.example.BookYourJam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookYourJamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookYourJamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BookYourJam/BookYourJamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BookYourJam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookYourJamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
